127133e51ddf21cce9795c0439dfa761eb884486
root <root@999cf97afb2c.(none)>
2017-12-28 15:32:13 +0000

    latest

--- app.py
+++ app.py
@@ -23,22 +23,13 @@ def TF_ohlc(df, tf):
     return ret.dropna()
 
 def mainaction():
-    #histdataPath="histdata/DAT_ASCII_USDJPY_M1_2015.csv"
-    #return TF_ohlc(read_histdata(histdataPath), 'H')
-    dat = """ts;A;B;C
-    2015-01-01 00:00:00;27451873;29956800;113
-    2015-01-01 01:00:00;20259882;17906600;76
-    2015-01-01 02:00:00;11592256;12311600;48
-    2015-01-01 03:00:00;11795562;11750100;50
-    2015-01-01 04:00:00;9396718;10203900;43
-    2015-01-01 05:00:00;14902826;14341100;53"""
-    df = pd.read_csv(pd.compat.StringIO(dat), sep=';', index_col='ts', parse_dates=True)
-    return df
+    histdataPath="histdata/DAT_ASCII_USDJPY_M1_2015.csv"
+    return TF_ohlc(read_histdata(histdataPath), 'H')
 
 @route("/")
 def index():
     df=mainaction()
-    chart = ph.serialize(df, render_to='my-chart', output_type='json')
+    chart = ph.serialize(df, chart_type="stock", title="chart", render_to='my-chart', output_type='json', grid=True)
     return template("charts.tpl", chart=chart)
 
 index()
--- base.tpl
+++ base.tpl
@@ -2,7 +2,8 @@
 <head>
 <title>chart</title>
 <link href="./bootstrap/css/bootstrap.css" rel="stylesheet">
-<script src="http://code.highcharts.com/highcharts.js"></script>
+<!-- <script src="http://code.highcharts.com/highcharts.js"></script> --->
+<script src="https://code.highcharts.com/stock/highstock.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="http://code.highcharts.com/modules/exporting.js"></script>
 <script src="./bootstrap/js/bootstrap.js"></script>
